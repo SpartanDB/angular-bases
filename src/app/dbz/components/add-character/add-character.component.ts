b8bcf52a823fe212bf8d999f9d12f2c6e3b037b9
Daniel Borrome <dborrome@gmail.com>
2023-04-26 23:51:55 -0400

    Fin seccion 5

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,26 @@
+import { Character } from './../../interfaces/character.interface';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css'],
+})
+export class AddCharacterComponent {
+  @Output()
+  public onNweCharacter: EventEmitter<Character> = new EventEmitter();
+
+  public character: Character = {
+    name: '',
+    power: 0,
+  };
+
+  emitCharacter(): void {
+    console.log(this.character);
+    if (this.character.name.length === 0) return;
+
+    this.onNweCharacter.emit(this.character);
+
+    this.character = { name: '', power: 0 };
+  }
+}
